bc6575a5c65449c06dea0be25f610dfdbf1a2bc5
JP La Torre <latorre.jp@gmail.com>
2017-06-01 17:06:14 -0700

    rev2

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World - this is a new production app!</p>'; ?> 
+ <?php echo '<p>Hello World - this is a new production app - rev2!</p>'; ?> 
  </body>
 </html>
