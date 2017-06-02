1921394ce066cfd2d9884f05a621625ebafa29d2
JP La Torre <latorre.jp@gmail.com>
2017-06-02 11:20:22 -0700

    staging reset

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Staging Branch</title>
  </head>
  <body>
- <?php echo '<p>Hello World - this is a NEW staging app!</p>'; ?> 
+ <?php echo '<p>Hello World - this is a staging app!</p>'; ?> 
  </body>
 </html>
