c7dc831b80843b3a47868092cbcdfe75cb57265c
JP La Torre <latorre.jp@gmail.com>
2017-06-01 17:58:55 -0700

    new prod build

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World - this is a new production app!</p>'; ?> 
+ <?php echo '<p>Hello World - this is a new production app - NEW PROD BUILD!</p>'; ?> 
  </body>
 </html>
