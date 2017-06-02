935e395193b9bf64135844cc31ed4b39fe6019e2
JP La Torre <latorre.jp@gmail.com>
2017-06-01 17:11:19 -0700

    dev reset

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World - this is a dev app that has been updated!</p>'; ?> 
+ <?php echo '<p>Hello World - this is a dev app!</p>'; ?> 
  </body>
 </html>
