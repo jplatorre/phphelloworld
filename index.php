67e102d48314d29015cf226b7b2101f2b896396d
JP La Torre <latorre.jp@gmail.com>
2017-06-01 17:01:48 -0700

    new update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World - this is a dev app!</p>'; ?> 
+ <?php echo '<p>Hello World - this is a dev app that has been updated!</p>'; ?> 
  </body>
 </html>
